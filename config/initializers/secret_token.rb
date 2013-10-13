596abed4d81872ccea7bef93705b55a33cb1a85f
Rafael Alonso <rafael.hantavirus@gmail.com>
2013-10-12 22:52:16 -0300

    my first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'ce491d46ec1fc11af70dfa50a9e329a8df2dd68ec89c383078d4f603b8fcf458dc3bf87efe4d8a74f8a8a382e53c9606c61df38eba9c56cc81408c8ce2054a80'
